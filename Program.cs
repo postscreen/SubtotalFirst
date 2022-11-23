967095053bc9c7562c80e56526dd1a7a74910224
Sergey M <berserker992@gmail.com>
2022-11-24 00:45:28 +1100

    First version of application

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Prepare supported variables
+int i = 0;
+List<string> incomingBuffer =  new List<string>();
+
+// Ethernal loop for collect incoming strings 
+while(true){
+
+    // Message for each session and save data from session also
+    Console.Clear();
+    Console.WriteLine($"Pleace, insert {i} array item: ");
+    var nextString = Console.ReadLine();
+
+    // Check that new string isn't null
+    if(nextString != null)
+        incomingBuffer.Add(nextString);
+
+    // User can stop or continuous
+    Console.Clear();
+    Console.WriteLine($"ESC to exit, ANYKEY to continuous ... ");
+    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+        break;
+
+    // Counter for easily understanding session' number
+    i++;
+
+}
+
+// Clear all previous interface info
+Console.Clear();
+
+// Prepare array, clear old data
+string[] arrayOfData = incomingBuffer.ToArray();
+incomingBuffer.Clear();
+
+// Show items that like to task' conditions
+Console.WriteLine($"We are have element that length is <= 3: ");
+for(i = 0; i < arrayOfData.Length; i++)
+    if(arrayOfData[i].Length <= 3)
+        Console.WriteLine(arrayOfData[i]);
